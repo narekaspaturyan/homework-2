2362453730333ebe3639568e612fb0f8edb1385a
narekaspaturyan <58630709+narekaspaturyan@users.noreply.github.com>
2019-12-13 18:24:55 +0400

    Add files via upload

---
+++ homework2 ex 11.js
@@ -0,0 +1,32 @@
+var int = 2;
+lastdigit= int% 10;
+var last= int.toString().split('').slice(1).join('');
+var firstdigit = (''+int)[0];
+var res = last.toString().split('').slice(0,-1).join('');
+
+if (lastdigit==int){
+    console.log(int)
+}else{ console.log(lastdigit+res+firstdigit)};
+
+
+
+var int = 13;
+lastdigit= int% 10;
+var last= int.toString().split('').slice(1).join('');
+var firstdigit = (''+int)[0];
+var res = last.toString().split('').slice(0,-1).join('');
+
+if (lastdigit==int){
+    console.log(int)
+}else{ console.log(lastdigit+res+firstdigit)};
+
+
+var int = 895796;
+lastdigit= int% 10;
+var last= int.toString().split('').slice(1).join('');
+var firstdigit = (''+int)[0];
+var res = last.toString().split('').slice(0,-1).join('');
+
+if (lastdigit==int){
+    console.log(int)
+}else{ console.log(lastdigit+res+firstdigit)};
