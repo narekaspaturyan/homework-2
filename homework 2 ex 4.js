01f2daa11e53f55a201abc6dc58de4eb5dbebc53
narekaspaturyan <58630709+narekaspaturyan@users.noreply.github.com>
2019-12-13 16:55:51 +0400

    Add files via upload

---
+++ homework 2 ex 4.js
@@ -0,0 +1,40 @@
+var int = 367;
+lastdigit= int% 10;
+var fist= int.toString().split('').slice(0, -1).join('')
+var newNum = Number(int)
+if(lastdigit===0){
+    console.log(int);
+}else if (lastdigit==int){
+    console.log(int);
+} else{console.log(lastdigit+fist)};
+
+
+var int = 1002;
+lastdigit= int% 10;
+var fist= int.toString().split('').slice(0, -1).join('')
+var newNum = Number(int)
+if(lastdigit===0){
+    console.log(int);
+}else if (lastdigit==int){
+    console.log(int);
+} else{console.log(lastdigit+fist)};
+
+var int = 250;
+lastdigit= int% 10;
+var fist= int.toString().split('').slice(0, -1).join('')
+var newNum = Number(int)
+if(lastdigit===0){
+    console.log(int);
+}else if (lastdigit==int){
+    console.log(int);
+} else{console.log(lastdigit+fist)};
+
+var int = 8;
+lastdigit= int% 10;
+var fist= int.toString().split('').slice(0, -1).join('')
+var newNum = Number(int)
+if(lastdigit===0){
+    console.log(int);
+}else if (lastdigit==int){
+    console.log(int);
+} else{console.log(lastdigit+fist)};
